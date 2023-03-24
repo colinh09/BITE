597b1a952bdd3487957d326501a379b2e8a4bef5
colinh09 <colin.hwang@cooper.edu>
2023-03-24 01:42:04 -0400

    testing testing

--- Backend/service.py
+++ Backend/service.py
@@ -7,10 +7,7 @@ import time
 client = googlemaps.Client('AIzaSyCpNT8X2EQ48kkPJEvAuLCWBYqPkyApfC0')
 
 # NYC Zipcodes to search
-zipcodes = [10001, 10002, 10003, 10004, 10005, 10006, 10007, 10009, 10010, 10011, 10012, 10013, 10014, 10015, 
-            10016, 10017, 10018, 10019, 10020, 10021, 10022, 10023, 10024, 10025, 10026, 10027, 10028, 10029, 
-            10030, 10031, 10032, 10033, 10034, 10035, 10036, 10037, 10038, 10039, 10040, 10041, 10044, 10045, 
-            10048, 10055, 10060, 10069, 10090, 10095, 10098, 10099, 10103, 10104, 10105, 10106, 10107, 10110]
+zipcodes = [10001, 10002, 10003]
 
 
 # Ratings to search
@@ -35,17 +32,27 @@ with open(output_file, 'w', newline='') as file:
 
 count = 0
 
+# WHY ARE THERE SO MANY RESTAURANTS WITH CHINESE LETTERS???
+# -*- coding: utf-8 -*-
+def isEnglish(s):
+    try:
+        s.encode(encoding='utf-8').decode('ascii')
+    except UnicodeDecodeError:
+        return False
+    else:
+        return True
+
 # Loop through each zipcode and rating
 for zipcode in zipcodes:
     location = geolocator.geocode(zipcode)
     # Make the API request
-    time.sleep(2)
-    places = client.places(query='restaurant', location=(location.latitude, location.longitude), radius = 1000, type='restaurant', language='en')
+
+    places = client.places(query='restaurant', location=(location.latitude, location.longitude), radius = 1000, type='restaurant', language='english')
     # Loop through each result and write to the CSV file
     with open(output_file, 'a', newline='') as file:
             writer = csv.writer(file)
             for place in places['results']:
-                if not place['name'] in restaurants_seen:
+                if not place['name'] in restaurants_seen and isEnglish(place['name']):
                     restaurants_seen.append(place['name'])
                     writer.writerow([place['name'], place['formatted_address']])
     
