1e9d63f2c91cf5835c4aa724cf3cc7c4b4c478d7
colinh09 <colin.hwang@cooper.edu>
2023-03-31 12:39:23 -0400

    uhhh yeah

--- Backend/populate_db.py
+++ Backend/populate_db.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://colin:boobadiboo@cluster0.zqayzpq.mongodb.net/?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://colin:<password>@cluster0.zqayzpq.mongodb.net/?retryWrites=true&w=majority")
 db = client.BITE
 restaurant_collection = db["restaurants"]
 
@@ -11,9 +11,9 @@ data = pd.read_csv("restaurant_data.csv")
 # Iterate through each row and insert into the 'restaurants' collection
 for index, row in data.iterrows():
     restaurant_document = {
-        "name": row["name"],
-        "location": row["address"],
-        "average_user_rating": row["rating"],
-        "average_price_rating": row["price_level"]
+        "name": row["Name"],
+        "location": row["Address"],
+        "average_user_rating": row["Rating [1-5]"],
+        "average_price_rating": row["Price Level [1-4]"]
     }
     restaurant_collection.insert_one(restaurant_document)
